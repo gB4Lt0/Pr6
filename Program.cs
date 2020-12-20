d1a8bb1e7fc88a56ea74ba569525dcbd1038befc
gB4Lt0 <czarenko.roman@vu.cdu.edu.ua>
2020-12-20 18:38:19 +0200

    Додано підрахунок суми чисел і виведення результату

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace GitLesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10={0}", x+y);
             Console.ReadKey();
+
         }
     }
 }
